5c38a7a28498cf964c58985a78db98b15153b37b
caojen <caojen@mail2.sysu.edu.cn>
2021-05-11 01:33:29 +0800

    add command and generate module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,11 @@ import { ProjectModule } from './project/project.module';
 import { DbModule } from './db/db.module';
 import { FsModule } from './fs/fs.module';
 import { FileModule } from './file/file.module';
+import { CommandModule } from './command/command.module';
+import { GenerateModule } from './generate/generate.module';
 
 @Module({
-  imports: [ProjectModule, DbModule, FsModule, FileModule],
+  imports: [ProjectModule, DbModule, FsModule, FileModule, CommandModule, GenerateModule],
   controllers: [AppController],
   providers: [AppService],
 })
