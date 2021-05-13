2d9c0550e69bbb1215bf55a735fee3abc3326da0
caojen <caojen@mail2.sysu.edu.cn>
2021-05-13 17:09:15 +0800

    throw output if something went wrong

--- src/command/command.service.ts
+++ src/command/command.service.ts
@@ -7,13 +7,15 @@ import * as process from 'process';
 export class CommandService {
   run(command: string, args?: string[]): string {
     const p = child_process.spawnSync(command, args);
-    if(p.error) {
+    if(p.status !== 0 || p.error || p.stderr.length > 0) {
       throw new HttpException({
         msg: '执行命令出错',
         e: p.error,
         command,
-        args
-      }, 500);
+        args,
+        stdout: p.stdout.toString(),
+        stderr: p.stderr.toString()
+      }, 200);
     } else {
       return p.stdout?.toString();
     }
